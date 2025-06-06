21ba26d0e27eb705bbc49824b19147cfc14b09b0
Michael van der Loo <michaeljvanderloo@gmail.com>
2025-06-05 23:50:25 -0400

    include new solvers in mod

--- src/solver/mod.rs
+++ src/solver/mod.rs
@@ -1,5 +1,9 @@
+pub mod arc_consistency;
 pub mod backtracking;
+pub mod forward_checking;
 pub mod heuristics;
 pub mod utils;
 
+pub use arc_consistency::ArcConsistencySolver;
 pub use backtracking::BacktrackingSolver;
+pub use forward_checking::ForwardCheckingSolver;
